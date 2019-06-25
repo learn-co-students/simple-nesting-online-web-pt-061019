83360a38ace596f6b601c8bb10b57b865108abbc
Lantz Warrick <betalantz@gmail.com>
2019-06-25 02:34:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
 		 :known_for => "Ruby",
 		 :languages => ["LISP", "C"]
 	 }
-
+   programmer_hash
 end
 
 def changing_alan
@@ -106,7 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 
 end
 
@@ -126,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+   programmer_hash
 end
